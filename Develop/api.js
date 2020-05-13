6821f9932907a400202154c590fa0fae746e9e1d
Erika <erika.paige.handley@gmail.com>
2020-05-13 08:52:44 -0700

    created a testfile

--- Develop/api.js
+++ Develop/api.js
@@ -3,40 +3,37 @@ const { prompt } = require('inquirer')
 //npm 'axios' package
 const axios = require('axios')
 
-//running specific axios request methos
-//prompt user for username
-prompt([
-  {
-    type: 'input',
-    name: 'userProfile',
-    message: 'GitHub Username:'
-  }
-])
-  .then(userProfile => {
-    console.log(userProfile)
-    //loop over different properties of an object to get to userProfile (key)
-    for (const name in userProfile) {
-      //return the value of data at the userProfile (key)
-      console.log(name[userProfile])
-      //using key data, write axios request for user profile from GitHub
-      axios.get(`https://api.github.com/users/${userProfile[name]}`)
-        .then(data => {
-          console.log(data)
-          // axios.get(`https://api.github.com/users/avatar_url`)
-          //   .then(data => {
-          //     console.log
-          //   })
-        })
-        .catch(err => console.log(err))
-    }
-  })
-  .catch(err => console.log(err))
-
-
+//create a varaible to set equal to API data
 // const api = {
-//   getUser(username) {
+  
+  //prompt user for username
+  prompt([
+    {
+      type: 'input',
+      name: 'userProfile',
+      message: 'GitHub Username:'
+  }
+  ])
+    .then(userProfile => {
+      console.log(userProfile)
+      //loop over different properties of an object to get to userProfile (key)
+      for (const name in userProfile) {
+        //return the value of data at the userProfile (key)
+        console.log(name[userProfile])
+        //using key data, write axios request for user profile from GitHub
+        axios.get(`https://api.github.com/users/${userProfile[name]}`)
+          .then(data => {
+            console.log(data)
+            //need to pull in their github image
+            // axios.get(`https://api.github.com/users/avatar_url`)
+            //   .then(data => {
+            //     console.log
+            //   })
+          })
+          .catch(err => console.log(err))
+      }
+    })
+      .catch(err => console.log(err))
+}
 
-//   }
-// };
-
-// module.exports = api;
+// module.exports = api
